9243bfe57a6744dab828d70ade1b7ce4dd892443
Robert <mcfaddenbones@gmail.com>
2025-08-20 20:53:49 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int hello1Function(string name1);
+int hello2Function(string name2);
+int main()
+{
+  string name1="your name1";
+  hello1Function(name1);
+  string name2="your name2";
+  hello2Function(name2);
+  return 0;
+}
+
+/*
+* hello1Function.cpp
+*
+*/
+
+int hello1Function(string name1)
+{
+  cout << "hello World " << name1 << endl;
+  return 0;
+}
+
+/*
+* hello2Function.cpp
+*
+*/
+
+int hello2Function(string name2)
+{
+  cout << "hello World " << name2 << endl;
+  return 0;
+}
